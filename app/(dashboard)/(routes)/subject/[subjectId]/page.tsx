2c2bad22a09eae0b45fa8e2f1189db9aba836e97
John Choque <johnchque.tech@gmail.com>
2024-02-25 13:46:19 +0100

    Adding title and description for Subjects.

--- app/(dashboard)/(routes)/subject/[subjectId]/page.tsx
+++ app/(dashboard)/(routes)/subject/[subjectId]/page.tsx
@@ -1,7 +1,87 @@
-const CourseIdPage = () => {
-    return (<div>
-        COURSE ID PAGE
-    </div> );
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { CircleDollarSign, File, LayoutDashboard, ListChecks } from "lucide-react";
+
+import { db } from "@/lib/db";
+// import { IconBadge } from "@/components/icon-badge";
+// import { Banner } from "@/components/banner";
+
+import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
+// import { ImageForm } from "./_components/image-form";
+// import { CategoryForm } from "./_components/category-form";
+// import { PriceForm } from "./_components/price-form";
+// import { AttachmentForm } from "./_components/attachment-form";
+// import { ChaptersForm } from "./_components/chapters-form";
+// import { Actions } from "./_components/actions";
+
+const CourseIdPage = async ({
+  params
+}: {
+  params: { subjectId: string }
+}) => {
+  
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+  const course = await db.subject.findUnique({
+    where: {
+      id: params.subjectId,
+      userId
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+  ];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completionText = `(${completedFields}/${totalFields})`;
+
+  const isComplete = requiredFields.every(Boolean);
+
+  return (
+    <>
+      <div className="p-6">
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-y-2">
+            <h1 className="text-2xl font-medium">
+              Course setup
+            </h1>
+            <span className="text-sm text-slate-700">
+              Complete all fields {completionText}
+            </span>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+          <div>
+            <div className="flex items-center gap-x-2">
+              <h2 className="text-xl">
+                Customize your course
+              </h2>
+            </div>
+            <TitleForm
+              initialData={course}
+              subjectId={course.id}
+            />
+            <DescriptionForm
+              initialData={course}
+              subjectId={course.id}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+   );
 }
  
 export default CourseIdPage;
---
+++ app/api/subjects/[subjectId]/route.ts
@@ -0,0 +1,33 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+    req: Request,
+    { params }: { params: { subjectId: string } }
+) {
+    try {
+        const { userId } = auth();
+        const { subjectId } = params;
+        const values = await req.json();
+
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const course = await db.subject.update({
+            where: {
+                id: subjectId,
+                userId
+            },
+            data: {
+                ...values,
+            }
+        });
+
+        return NextResponse.json(course);
+    } catch (error) {
+        console.log("[COURSE_ID]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
